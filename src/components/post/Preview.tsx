323a1af85552140730fb3e740549324c74bb18e5
jyhisgood <jaiemf@gmail.com>
2024-02-05 23:09:32 +0900

    feat: Implement detail page editor and previewer

--- src/components/post/Preview.tsx
+++ src/components/post/Preview.tsx
@@ -1,13 +1,22 @@
 'use client';
 
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+
 import type { Post } from '@/types';
 
 const Preview = ({ title, content }: Post.Partial) => {
   return (
-    <>
-      <h1>{title}</h1>
-      <p>{content}</p>
-    </>
+    <Editor
+      initialValue={content || ' '} // 글 수정 시 사용
+      initialEditType="markdown" // wysiwyg & markdown
+      previewStyle={window.innerWidth > 1000 ? 'vertical' : 'tab'} // tab, vertical
+      hideModeSwitch={true}
+      height="100%"
+      theme={''} // '' & 'dark'
+      usageStatistics={false}
+      useCommandShortcut={true}
+    />
   );
 };
 
